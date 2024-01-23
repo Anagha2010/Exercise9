c56ae90a83aa9d75cb4b59bbc134fbde33dbf015
Anagha <mailsforanagha@gmail.com>
2024-01-23 23:19:28 +0000

    adding types file to git repo

---
+++ types_practice.py
@@ -0,0 +1,9 @@
+# Following two statements initialize two variables with strings
+first_name = "Anagha"
+last_name = "Jawalekar"
+# Following statement prints the variable values assigned above
+print(first_name, last_name)
+# Following statement assigns values to a list
+full_name = [first_name, last_name]
+# Following statement prints the two values in the list
+print(full_name[0], full_name[1])
